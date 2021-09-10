151c97c399358eb2c8014644567805d96b2a6c53
Kingnova5546 <marquis5546@yahoo.com>
2021-09-10 16:53:20 -0700

    Yes

--- Real First Game/Assets/Scripts/Chest.cs
+++ Real First Game/Assets/Scripts/Chest.cs
@@ -6,11 +6,13 @@ public class Chest : Collectable
 {
     public Sprite emptyChest;
     public int goldAmount = 5;
+    public Animator animator;
     protected override void OnCollect()
     {
         if (!collected)
         {
             collected = true;
+            animator.SetTrigger("ChestCollect");
             GetComponent<SpriteRenderer>().sprite = emptyChest;
             GameManager.instance.ShowText($"+ {goldAmount} Gold", 25, Color.yellow, transform.position, Vector3.up * 10, 1.5f);
 
